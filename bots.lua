f070077fd0ec911065e9291006f26a286b7a1a87
oc9x97 <142158555+oc9x97@users.noreply.github.com>
2023-09-23 12:21:27 -0400

    Update bots.lua

--- bots.lua
+++ bots.lua
@@ -6,7 +6,7 @@
 -- SO GOODLUCK COPYING THAT SKIDS
 ------------------------------------
 
-getgenv().host = "oc9x97"
+getgenv().host = "Username"
 getgenv().alts = {"alt1", "alt2"} -- Optional to fill out
 getgenv().defaultfps = 60
 getgenv().prefix = ""
